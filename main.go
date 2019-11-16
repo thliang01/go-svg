446daf822795cdf9d38602f06f2bb6814d2d8433
thliang01 <thliang01@gmail.com>
2019-11-16 18:21:19 +0800

    update muiti-kidSVG
    Signed-off-by: thliang01 <thliang01@gmail.com>

--- main.go
+++ main.go
@@ -10,20 +10,8 @@ import (
 )
 
 var (
-	height = 10000
+	height = 1000
 	width  = 10000
-	/*
-		Id       int
-		Name     string
-		Age      int
-		Gender   string
-		IdNumber string
-		Father   string
-		Mother   string
-		Couple   string
-		Address  string
-
-	*/
 )
 
 type idcard struct {
@@ -60,17 +48,15 @@ func CoupleSVG(w http.ResponseWriter, req *http.Request) {
 	s := svg.New(w)
 	s.Start(width, height)
 
-	for i := 0; i <= width; i = i + 50 {
-		s.Line(0, i, width, i, "fill:black;stroke:black")
-	}
-
-	for j := 0; j <= height; j = j + 10 {
-		s.Line(j, 0, j, height, "fill:black;stroke:gray;stroke-dasharray:2")
-	}
-
 	/*
-		echo '<rect x="'.($myfather*$mygrid-($mygrid/2)).'" y="'.($mygrid*2-($mygrid/2)).'" width="'.$mygrid.'" height="'.$mygrid.'"
-		style="fill:blue;stroke:pink;stroke-width:5;fill-opacity:0.1;stroke-opacity:1" />';
+		for i := 0; i <= width; i = i + 50 {
+			s.Line(0, i, width, i, "fill:black;stroke:black")
+		}
+
+		for j := 0; j <= height; j = j + 10 {
+			s.Line(j, 0, j, height, "fill:black;stroke:gray;stroke-dasharray:2")
+		}
+
 	*/
 
 	s.Square(125, 125, 250, "fill:blue")
@@ -85,13 +71,16 @@ func SingleManSVG(w http.ResponseWriter, req *http.Request) {
 	s := svg.New(w)
 	s.Start(width, height)
 
-	for i := 0; i <= width; i = i + 50 {
-		s.Line(0, i, width, i, "fill:black;stroke:black")
-	}
+	/*
+		for i := 0; i <= width; i = i + 50 {
+			s.Line(0, i, width, i, "fill:black;stroke:black")
+		}
 
-	for j := 0; j <= height; j = j + 10 {
-		s.Line(j, 0, j, height, "fill:black;stroke:gray;stroke-dasharray:2")
-	}
+		for j := 0; j <= height; j = j + 10 {
+			s.Line(j, 0, j, height, "fill:black;stroke:gray;stroke-dasharray:2")
+		}
+
+	*/
 
 	s.Square(125, 125, 250, "fill:blue")
 	s.End()
@@ -102,13 +91,16 @@ func SingleWomanSVG(w http.ResponseWriter, req *http.Request) {
 	s := svg.New(w)
 	s.Start(width, height)
 
-	for i := 0; i <= width; i = i + 50 {
-		s.Line(0, i, width, i, "fill:black;stroke:black")
-	}
+	/*
+		for i := 0; i <= width; i = i + 50 {
+			s.Line(0, i, width, i, "fill:black;stroke:black")
+		}
 
-	for j := 0; j <= height; j = j + 10 {
-		s.Line(j, 0, j, height, "fill:black;stroke:gray;stroke-dasharray:2")
-	}
+		for j := 0; j <= height; j = j + 10 {
+			s.Line(j, 0, j, height, "fill:black;stroke:gray;stroke-dasharray:2")
+		}
+
+	*/
 
 	s.Circle(250, 250, 125, "fill:red")
 	s.End()
@@ -310,17 +302,32 @@ func ThreeKidSVG(w http.ResponseWriter, req *http.Request) {
 	s := svg.New(w)
 	s.Start(width, height)
 
-	for i := 0; i <= width; i = i + 50 {
-		s.Line(0, i, width, i, "fill:black;stroke:black")
+	/*
+		for i := 0; i <= width; i = i + 50 {
+			s.Line(0, i, width, i, "fill:black;stroke:black")
+		}
+
+		for j := 0; j <= height; j = j + 10 {
+			s.Line(j, 0, j, height, "fill:black;stroke:gray;stroke-dasharray:2")
+		}
+	*/
+
+	var err error
+	Db, err = sql.Open("postgres", "user=tree1 dbname=tree1 password=tree1")
+	if err != nil {
+		panic(err)
 	}
 
-	for j := 0; j <= height; j = j + 10 {
-		s.Line(j, 0, j, height, "fill:black;stroke:gray;stroke-dasharray:2")
+	row, err := Db.Query("select id, name, age , gender, id_number, father, mother, address from idcard")
+	if err != nil {
+		return
 	}
 
-	s.Square(125, 125, 250, "fill:blue")
+	var id idcard
+
+	// s.Square(125, 125, 250, "fill:blue")
 	s.Line(375, 250, 1875, 250, "stroke:black;stroke-width:20;stroke-linecap:butt")
-	s.Circle(2000, 250, 125, "fill:red")
+	// s.Circle(2000, 250, 125, "fill:red")
 
 	s.Line(1125, 250, 1125, 500, "stroke:black;stroke-width:20;stroke-linecap:butt")
 	s.Line(250, 500, 2000, 500, "stroke:black;stroke-width:20;stroke-linecap:butt")
@@ -328,10 +335,94 @@ func ThreeKidSVG(w http.ResponseWriter, req *http.Request) {
 	s.Line(1125, 500, 1125, 750, "stroke:black;stroke-width:20;stroke-linecap:butt")
 	s.Line(2000, 500, 2000, 750, "stroke:black;stroke-width:20;stroke-linecap:butt")
 
-	s.Circle(250, 875, 125, "fill:green")
-	s.Circle(1125, 875, 125, "fill:green")
-	s.Circle(2000, 875, 125, "fill:green")
+	// s.Circle(250, 875, 125, "fill:green")
+	// s.Circle(1125, 875, 125, "fill:green")
+	// s.Circle(2000, 875, 125, "fill:green")
 
+	for row.Next() {
+		err = row.Scan(&id.Id, &id.Name, &id.Age, &id.Gender,
+			&id.IdNumber, &id.Father, &id.Mother, &id.Address)
+		if err != nil {
+			return
+		}
+		//log.Println(id.Id, id.Name, id.Age, id.Gender,
+		//	id.IdNumber, id.Father, id.Mother, id.Address)
+		if id.Id == 1 {
+			s.Text(225, 250, id.Name, "text-anchor:middle;font-size:30px")
+			// log.Println(id.Age)
+			s.Text(225, 280, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Text(225, 310, id.IdNumber, "text-anchor:middle;font-size:30px")
+			// log.Println(id.Gender)
+			s.Square(125, 125, 250, "fill:none;stroke:black")
+		} else if id.Id == 2 {
+			s.Text(1975, 250, id.Name, "text-anchor:middle;font-size:30px")
+			// log.Println(id.Age)
+			s.Text(1975, 280, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Text(1975, 310, id.IdNumber, "text-anchor:middle;font-size:30px")
+			// log.Println(id.Gender)
+			s.Circle(2000, 250, 125, "fill:none;stroke:black")
+			// log.Println(id.Id)
+		} else if id.Id == 3 {
+			// log.Println(id.Age)
+			// log.Println(string(id.Age))
+			// log.Println(id.Gender)
+			// s.Circle(250, 875, 125, "fill:green")
+			s.Text(250, 875, id.Name, "text-anchor:middle;font-size:30px")
+			s.Text(250, 905, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Text(250, 935, id.IdNumber, "text-anchor:middle;font-size:30px")
+			if id.Gender == "男" {
+				s.Square(125, 750, 250, "fill:none;stroke:black")
+			} else {
+				s.Circle(250, 875, 125, "fill:none;stroke:black")
+			}
+		} else if id.Id == 4 {
+			// log.Println(id.Age)
+			// log.Println(string(id.Age))
+			// log.Println(id.Gender)
+			// s.Circle(2000, 875, 125, "fill:green")
+			s.Text(1125, 875, id.Name, "text-anchor:middle;font-size:30px")
+			s.Text(1125, 905, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Text(1125, 935, id.IdNumber, "text-anchor:middle;font-size:30px")
+			if id.Gender == "男" {
+				s.Square(1000, 750, 250, "fill:none;stroke:black")
+			} else {
+				s.Circle(1125, 875, 125, "fill:none;stroke:black")
+			}
+
+		} else if id.Id == 5 {
+			// log.Println(id.Age)
+			// log.Println(string(id.Age))
+			// log.Println(id.Gender)
+			// s.Circle(2000, 875, 125, "fill:green")
+			s.Text(2000, 875, id.Name, "text-anchor:middle;font-size:30px")
+			s.Text(2000, 905, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Text(2000, 935, id.IdNumber, "text-anchor:middle;font-size:30px")
+			if id.Gender == "男" {
+				s.Square(1875, 750, 250, "fill:none;stroke:black")
+			} else {
+				s.Circle(2000, 875, 125, "fill:none;stroke:black")
+			}
+
+		}
+	}
+
+	/*
+		s.Square(125, 125, 250, "fill:blue")
+		s.Line(375, 250, 1875, 250, "stroke:black;stroke-width:20;stroke-linecap:butt")
+		s.Circle(2000, 250, 125, "fill:red")
+
+		s.Line(1125, 250, 1125, 500, "stroke:black;stroke-width:20;stroke-linecap:butt")
+		s.Line(250, 500, 2000, 500, "stroke:black;stroke-width:20;stroke-linecap:butt")
+		s.Line(250, 500, 250, 750, "stroke:black;stroke-width:20;stroke-linecap:butt")
+		s.Line(1125, 500, 1125, 750, "stroke:black;stroke-width:20;stroke-linecap:butt")
+		s.Line(2000, 500, 2000, 750, "stroke:black;stroke-width:20;stroke-linecap:butt")
+
+		s.Circle(250, 875, 125, "fill:green")
+		s.Circle(1125, 875, 125, "fill:green")
+		s.Circle(2000, 875, 125, "fill:green")
+
+
+	*/
 	//s.Circle(1125, 875, 125 ,"fill:green")
 
 	s.End()
@@ -342,17 +433,33 @@ func FourKidSVG(w http.ResponseWriter, req *http.Request) {
 	s := svg.New(w)
 	s.Start(width, height)
 
-	for i := 0; i <= width; i = i + 50 {
-		s.Line(0, i, width, i, "fill:black;stroke:black")
+	/*
+		for i := 0; i <= width; i = i + 50 {
+			s.Line(0, i, width, i, "fill:black;stroke:black")
+		}
+
+		for j := 0; j <= height; j = j + 10 {
+			s.Line(j, 0, j, height, "fill:black;stroke:gray;stroke-dasharray:2")
+		}
+
+	*/
+
+	var err error
+	Db, err = sql.Open("postgres", "user=tree1 dbname=tree1 password=tree1")
+	if err != nil {
+		panic(err)
 	}
 
-	for j := 0; j <= height; j = j + 10 {
-		s.Line(j, 0, j, height, "fill:black;stroke:gray;stroke-dasharray:2")
+	row, err := Db.Query("select id, name, age , gender, id_number, father, mother, address from idcard")
+	if err != nil {
+		return
 	}
 
-	s.Square(125, 125, 250, "fill:blue")
+	var id idcard
+
+	// s.Square(125, 125, 250, "fill:blue")
 	s.Line(375, 250, 1875, 250, "stroke:black;stroke-width:20;stroke-linecap:butt")
-	s.Circle(2000, 250, 125, "fill:red")
+	// s.Circle(2000, 250, 125, "fill:red")
 
 	s.Line(1125, 250, 1125, 500, "stroke:black;stroke-width:20;stroke-linecap:butt")
 	s.Line(250, 500, 2000, 500, "stroke:black;stroke-width:20;stroke-linecap:butt")
@@ -362,11 +469,94 @@ func FourKidSVG(w http.ResponseWriter, req *http.Request) {
 	s.Line(1562, 500, 1562, 750, "stroke:black;stroke-width:20;stroke-linecap:butt")
 	s.Line(2000, 500, 2000, 750, "stroke:black;stroke-width:20;stroke-linecap:butt")
 
-	s.Circle(250, 875, 125, "fill:green")
+	for row.Next() {
+		err = row.Scan(&id.Id, &id.Name, &id.Age, &id.Gender,
+			&id.IdNumber, &id.Father, &id.Mother, &id.Address)
+		if err != nil {
+			return
+		}
+		//log.Println(id.Id, id.Name, id.Age, id.Gender,
+		//	id.IdNumber, id.Father, id.Mother, id.Address)
+		if id.Id == 1 {
+			s.Text(225, 250, id.Name, "text-anchor:middle;font-size:30px")
+			// log.Println(id.Age)
+			s.Text(225, 280, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Text(225, 310, id.IdNumber, "text-anchor:middle;font-size:30px")
+			// log.Println(id.Gender)
+			s.Square(125, 125, 250, "fill:none;stroke:black")
+		} else if id.Id == 2 {
+			s.Text(1975, 250, id.Name, "text-anchor:middle;font-size:30px")
+			// log.Println(id.Age)
+			s.Text(1975, 280, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Text(1975, 310, id.IdNumber, "text-anchor:middle;font-size:30px")
+			// log.Println(id.Gender)
+			s.Circle(2000, 250, 125, "fill:none;stroke:black")
+			// log.Println(id.Id)
+		} else if id.Id == 3 {
+			// log.Println(id.Age)
+			// log.Println(string(id.Age))
+			// log.Println(id.Gender)
+			// s.Circle(250, 875, 125, "fill:green")
+			s.Text(250, 875, id.Name, "text-anchor:middle;font-size:30px")
+			s.Text(250, 905, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Text(250, 935, id.IdNumber, "text-anchor:middle;font-size:30px")
+			if id.Gender == "男" {
+				s.Square(125, 750, 250, "fill:none;stroke:black")
+			} else {
+				s.Circle(250, 875, 125, "fill:none;stroke:black")
+			}
+		} else if id.Id == 4 {
+			// log.Println(id.Age)
+			// log.Println(string(id.Age))
+			// log.Println(id.Gender)
+			// s.Circle(2000, 875, 125, "fill:green")
+			//s.Circle(1125, 875, 125, "fill:green")
+			s.Text(687, 875, id.Name, "text-anchor:middle;font-size:30px")
+			s.Text(687, 905, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Text(687, 935, id.IdNumber, "text-anchor:middle;font-size:30px")
+			if id.Gender == "男" {
+				s.Square(562, 750, 250, "fill:none;stroke:black")
+			} else {
+				s.Circle(687, 875, 125, "fill:none;stroke:black")
+				// s.Circle(1125, 875, 125, "fill:none;stroke:black")
+			}
+
+		} else if id.Id == 5 {
+			// log.Println(id.Age)
+			// log.Println(string(id.Age))
+			// log.Println(id.Gender)
+			// s.Circle(2000, 875, 125, "fill:green")
+			s.Text(1562, 875, id.Name, "text-anchor:middle;font-size:30px")
+			s.Text(1562, 905, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Text(1562, 935, id.IdNumber, "text-anchor:middle;font-size:30px")
+			if id.Gender == "男" {
+				s.Square(1437, 750, 250, "fill:none;stroke:black")
+			} else {
+				//s.Circle(1562, 875, 125, "fill:green")
+				s.Circle(1562, 875, 125, "fill:none;stroke:black")
+			}
+
+		} else if id.Id == 6 {
+			// log.Println(id.Age)
+			// log.Println(string(id.Age))
+			// log.Println(id.Gender)
+			// s.Circle(2000, 875, 125, "fill:green")
+			s.Text(2000, 875, id.Name, "text-anchor:middle;font-size:30px")
+			s.Text(2000, 905, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Text(2000, 935, id.IdNumber, "text-anchor:middle;font-size:30px")
+			if id.Gender == "男" {
+				s.Square(1875, 750, 250, "fill:none;stroke:black")
+			} else {
+				s.Circle(2000, 875, 125, "fill:none;stroke:black")
+			}
+
+		}
+	}
+	// s.Circle(250, 875, 125, "fill:green")
 	// s.Circle(1125, 875, 125, "fill:green")
-	s.Circle(687, 875, 125, "fill:green")
-	s.Circle(1562, 875, 125, "fill:green")
-	s.Circle(2000, 875, 125, "fill:green")
+	// s.Circle(687, 875, 125, "fill:green")
+	// s.Circle(1562, 875, 125, "fill:green")
+	// s.Circle(2000, 875, 125, "fill:green")
 
 	//s.Circle(1125, 875, 125 ,"fill:green")
 
@@ -378,17 +568,33 @@ func FiveKidSVG(w http.ResponseWriter, req *http.Request) {
 	s := svg.New(w)
 	s.Start(width, height)
 
-	for i := 0; i <= width; i = i + 50 {
-		s.Line(0, i, width, i, "fill:black;stroke:black")
+	var err error
+	Db, err = sql.Open("postgres", "user=tree1 dbname=tree1 password=tree1")
+	if err != nil {
+		panic(err)
 	}
 
-	for j := 0; j <= height; j = j + 10 {
-		s.Line(j, 0, j, height, "fill:black;stroke:gray;stroke-dasharray:2")
+	row, err := Db.Query("select id, name, age , gender, id_number, father, mother, address from idcard")
+	if err != nil {
+		return
 	}
 
-	s.Square(125, 125, 250, "fill:blue")
+	var id idcard
+
+	/*
+		for i := 0; i <= width; i = i + 50 {
+			s.Line(0, i, width, i, "fill:black;stroke:black")
+		}
+
+		for j := 0; j <= height; j = j + 10 {
+			s.Line(j, 0, j, height, "fill:black;stroke:gray;stroke-dasharray:2")
+		}
+
+	*/
+
+	// s.Square(125, 125, 250, "fill:blue")
 	s.Line(375, 250, 1875, 250, "stroke:black;stroke-width:20;stroke-linecap:butt")
-	s.Circle(2000, 250, 125, "fill:red")
+	// s.Circle(2000, 250, 125, "fill:red")
 
 	s.Line(1125, 250, 1125, 500, "stroke:black;stroke-width:20;stroke-linecap:butt")
 	s.Line(250, 500, 2000, 500, "stroke:black;stroke-width:20;stroke-linecap:butt")
@@ -398,13 +604,107 @@ func FiveKidSVG(w http.ResponseWriter, req *http.Request) {
 	s.Line(1562, 500, 1562, 750, "stroke:black;stroke-width:20;stroke-linecap:butt")
 	s.Line(2000, 500, 2000, 750, "stroke:black;stroke-width:20;stroke-linecap:butt")
 
-	s.Circle(250, 875, 125, "fill:green")
-	s.Circle(1125, 875, 125, "fill:green")
-	s.Circle(687, 875, 125, "fill:green")
-	s.Circle(1562, 875, 125, "fill:green")
-	s.Circle(2000, 875, 125, "fill:green")
+	// s.Circle(250, 875, 125, "fill:green")
+	// s.Circle(1125, 875, 125, "fill:green")
+	// s.Circle(687, 875, 125, "fill:green")
+	// s.Circle(1562, 875, 125, "fill:green")
+	// s.Circle(2000, 875, 125, "fill:green")
 
-	//s.Circle(1125, 875, 125 ,"fill:green")
+	// s.Circle(1125, 875, 125 ,"fill:green")
 
+	for row.Next() {
+		err = row.Scan(&id.Id, &id.Name, &id.Age, &id.Gender,
+			&id.IdNumber, &id.Father, &id.Mother, &id.Address)
+		if err != nil {
+			return
+		}
+		//log.Println(id.Id, id.Name, id.Age, id.Gender,
+		//	id.IdNumber, id.Father, id.Mother, id.Address)
+		if id.Id == 1 {
+			s.Text(225, 250, id.Name, "text-anchor:middle;font-size:30px")
+			// log.Println(id.Age)
+			s.Text(225, 280, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Text(225, 310, id.IdNumber, "text-anchor:middle;font-size:30px")
+			// log.Println(id.Gender)
+			s.Square(125, 125, 250, "fill:none;stroke:black")
+		} else if id.Id == 2 {
+			s.Text(1975, 250, id.Name, "text-anchor:middle;font-size:30px")
+			// log.Println(id.Age)
+			s.Text(1975, 280, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Text(1975, 310, id.IdNumber, "text-anchor:middle;font-size:30px")
+			// log.Println(id.Gender)
+			s.Circle(2000, 250, 125, "fill:none;stroke:black")
+			// log.Println(id.Id)
+		} else if id.Id == 3 {
+			// log.Println(id.Age)
+			// log.Println(string(id.Age))
+			// log.Println(id.Gender)
+			// s.Circle(250, 875, 125, "fill:green")
+			s.Text(250, 875, id.Name, "text-anchor:middle;font-size:30px")
+			s.Text(250, 905, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Text(250, 935, id.IdNumber, "text-anchor:middle;font-size:30px")
+			if id.Gender == "男" {
+				s.Square(125, 750, 250, "fill:none;stroke:black")
+			} else {
+				s.Circle(250, 875, 125, "fill:none;stroke:black")
+			}
+		} else if id.Id == 4 {
+			// log.Println(id.Age)
+			// log.Println(string(id.Age))
+			// log.Println(id.Gender)
+			// s.Circle(2000, 875, 125, "fill:green")
+			s.Text(687, 875, id.Name, "text-anchor:middle;font-size:30px")
+			s.Text(687, 905, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Text(687, 935, id.IdNumber, "text-anchor:middle;font-size:30px")
+			if id.Gender == "男" {
+				s.Square(562, 750, 250, "fill:none;stroke:black")
+			} else {
+				s.Circle(687, 875, 125, "fill:none;stroke:black")
+			}
+
+		} else if id.Id == 5 {
+			// log.Println(id.Age)
+			// log.Println(string(id.Age))
+			// log.Println(id.Gender)
+			// s.Circle(2000, 875, 125, "fill:green")
+			s.Text(1125, 875, id.Name, "text-anchor:middle;font-size:30px")
+			s.Text(1125, 905, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Text(1125, 935, id.IdNumber, "text-anchor:middle;font-size:30px")
+			if id.Gender == "男" {
+				s.Square(1000, 750, 250, "fill:none;stroke:black")
+			} else {
+				s.Circle(1125, 875, 125, "fill:none;stroke:black")
+			}
+
+		} else if id.Id == 6 {
+			// log.Println(id.Age)
+			// log.Println(string(id.Age))
+			// log.Println(id.Gender)
+			// s.Circle(2000, 875, 125, "fill:green")
+			s.Text(1562, 875, id.Name, "text-anchor:middle;font-size:30px")
+			s.Text(1562, 905, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Text(1562, 935, id.IdNumber, "text-anchor:middle;font-size:30px")
+			if id.Gender == "男" {
+				s.Square(1875, 750, 250, "fill:none;stroke:black")
+			} else {
+				s.Circle(1562, 875, 125, "fill:none;stroke:black")
+			}
+
+		} else if id.Id == 7 {
+			// log.Println(id.Age)
+			// log.Println(string(id.Age))
+			// log.Println(id.Gender)
+			// s.Circle(2000, 875, 125, "fill:green")
+			s.Text(2000, 875, id.Name, "text-anchor:middle;font-size:30px")
+			s.Text(2000, 905, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Text(2000, 935, id.IdNumber, "text-anchor:middle;font-size:30px")
+			if id.Gender == "男" {
+				s.Square(1875, 750, 250, "fill:none;stroke:black")
+			} else {
+				s.Circle(2000, 875, 125, "fill:none;stroke:black")
+			}
+
+		}
+	}
 	s.End()
 }
