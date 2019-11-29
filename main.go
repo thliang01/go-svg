4451e029eacdda13e65bfa6579d2776ab50e25a6
thliang01 <thliang01@gmail.com>
2019-11-29 18:11:53 +0800

    update ThreeKidSVG in main.go
    Signed-off-by: thliang01 <thliang01@gmail.com>

--- main.go
+++ main.go
@@ -23,8 +23,10 @@ var (
 	father     = "王大明"
 	mother     = "陳大美"
 	WIDTH      = children*grid + (children-1)*span
-	YPARENTORG = startx + grid/2
+	YPARENTORG = starty + grid/2
 	YCHILDORG  = YPARENTORG + grid + span
+	XPARENTORG = startx + grid/2
+	XCHILDORG  = startx + grid/2
 )
 
 type idcard struct {
@@ -58,7 +60,7 @@ func main() {
 		if err != nil {
 			return
 		}
-		log.Println(startx, starty, grid, span)
+		// log.Println(startx, starty, grid, span)
 	}
 
 	// var count int
@@ -68,40 +70,63 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(count, YPARENTORG, YCHILDORG)
+	children = count
+	WIDTH = children*grid + (children-1)*span
+	XPARENTORG = startx + grid/2
+	YPARENTORG = starty + grid/2
+	XCHILDORG = startx + grid/2
+	YCHILDORG = YPARENTORG + grid + span
+
+	fmt.Println(count, WIDTH, YPARENTORG, YCHILDORG, XPARENTORG)
+
+	// fmt.Println(count)
+
+	switch count {
+	case 1:
+		println("one kid")
+		http.Handle("/onekidsvg", http.HandlerFunc(OneKidSVG))
+		break
+
+	case 2:
+		println("two kid")
+		http.Handle("/twokidsvg", http.HandlerFunc(TwoKidSVG))
+		break
+
+	case 3:
+		println("three kid")
+		http.Handle("/threekidsvg", http.HandlerFunc(ThreeKidSVG))
+		break
+
+	case 4:
+		println("four kid")
+		http.Handle("/fourkidsvg", http.HandlerFunc(FourKidSVG))
+		break
+
+	case 5:
+		println("five kid")
+		http.Handle("/fivekidsvg", http.HandlerFunc(FiveKidSVG))
+		break
+
+	default:
+		println("Not a family")
+		break
+	}
+
 	/*
-
-		fmt.Println(count)
-
-		switch count {
-		case 1:
-			http.Handle("/onekidsvg", http.HandlerFunc(OneKidSVG))
-		case 2:
-			http.Handle("/twokidsvg", http.HandlerFunc(TwoKidSVG))
-		case 3:
-			http.Handle("/threekidsvg", http.HandlerFunc(ThreeKidSVG))
-		case 4:
-			http.Handle("/fourkidsvg", http.HandlerFunc(FourKidSVG))
-		case 5:
-			http.Handle("/fivekidsvg", http.HandlerFunc(FiveKidSVG))
-			}
-
-		err = http.ListenAndServe(":8080", nil)
-		if err != nil {
-				log.Fatal("ListenAndServe:", err)
-		}
+		http.Handle("/singlemansvg", http.HandlerFunc(SingleManSVG))
+		http.Handle("/singlewomansvg", http.HandlerFunc(SingleWomanSVG))
+		http.Handle("/couplesvg", http.HandlerFunc(CoupleSVG))
 
 	*/
+	/*
+		http.Handle("/onekidsvg", http.HandlerFunc(OneKidSVG))
+		http.Handle("/twokidsvg", http.HandlerFunc(TwoKidSVG))
+		http.Handle("/threekidsvg", http.HandlerFunc(ThreeKidSVG))
+		http.Handle("/fourkidsvg", http.HandlerFunc(FourKidSVG))
+		http.Handle("/fivekidsvg", http.HandlerFunc(FiveKidSVG))
 
-	http.Handle("/singlemansvg", http.HandlerFunc(SingleManSVG))
-	http.Handle("/singlewomansvg", http.HandlerFunc(SingleWomanSVG))
-	http.Handle("/couplesvg", http.HandlerFunc(CoupleSVG))
-	http.Handle("/onekidsvg", http.HandlerFunc(OneKidSVG))
-	http.Handle("/twokidsvg", http.HandlerFunc(TwoKidSVG))
-	http.Handle("/threekidsvg", http.HandlerFunc(ThreeKidSVG))
-	http.Handle("/fourkidsvg", http.HandlerFunc(FourKidSVG))
-	http.Handle("/fivekidsvg", http.HandlerFunc(FiveKidSVG))
-	err = http.ListenAndServe(":2003", nil)
+	*/
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
@@ -184,7 +209,7 @@ func OneKidSVG(w http.ResponseWriter, req *http.Request) {
 	var id idcard
 	w.Header().Set("Content-Type", "image/svg+xml")
 	s := svg.New(w)
-	s.Start(width, height)
+	s.Start(WIDTH, height)
 	s.Line(startx+grid, starty+grid/2, startx+grid+span, starty+grid/2, "stroke:black;stroke-width:20;stroke-linecap:butt")
 	s.Line(startx+grid+span/2, starty+grid/2, startx+grid+span/2, starty+grid/2+span, "stroke:black;stroke-width:20;stroke-linecap:butt")
 	for row.Next() {
@@ -194,10 +219,10 @@ func OneKidSVG(w http.ResponseWriter, req *http.Request) {
 			return
 		}
 		if id.Id == 1 {
-			s.Text(startx+(grid/4)*3, starty+grid/2, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
-			s.Square(startx+grid/2, starty+grid/4, grid/2, "fill:none;stroke:black")
+			s.Text(startx+(grid/4)*3, YPARENTORG, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Square(XPARENTORG, starty+grid/4, grid/2, "fill:none;stroke:black")
 		} else if id.Id == 2 {
-			s.Text(startx+(grid/4)*5+span, starty+grid/2, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Text(startx+(grid/4)*5+span, YPARENTORG, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
 			s.Circle(startx+(grid/4)*5+span, starty+grid/2, grid/4, "fill:none;stroke:black")
 		} else if id.Id == 3 {
 			s.Text(startx+grid+span/2, starty+(grid/4)*3+span, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
@@ -214,7 +239,7 @@ func OneKidSVG(w http.ResponseWriter, req *http.Request) {
 func TwoKidSVG(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "image/svg+xml")
 	s := svg.New(w)
-	s.Start(width, height)
+	s.Start(WIDTH, height)
 	var err error
 	Db, err = sql.Open("postgres", "user=tree1 dbname=tree1 password=tree1")
 	if err != nil {
@@ -243,14 +268,14 @@ func TwoKidSVG(w http.ResponseWriter, req *http.Request) {
 			s.Text(startx+(grid/4)*5+span, starty+grid/2, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
 			s.Circle(startx+(grid/4)*5+span, starty+grid/2, grid/4, "fill:none;stroke:black")
 		} else if id.Id == 3 {
-			s.Text((grid/4)*3, starty+(grid/4)*3+span, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Text((grid/4)*3, YCHILDORG, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
 			if id.Gender == "男" {
 				s.Square(startx+grid/2, starty+grid/2+span, grid/2, "fill:none;stroke:black")
 			} else {
 				s.Circle(startx+(grid/4)*5+span, starty+grid/2+span+grid/4, grid/4, "fill:none;stroke:black")
 			}
 		} else if id.Id == 4 {
-			s.Text(startx+grid+span+grid/4, starty+(grid/4)*3+span, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Text(startx+grid+span+grid/4, YCHILDORG, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
 			if id.Gender == "男" {
 				s.Square(startx+grid+span, starty+grid/2+span, grid/2, "fill:none;stroke:black")
 			} else {
@@ -265,17 +290,6 @@ func ThreeKidSVG(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "image/svg+xml")
 	s := svg.New(w)
 	s.Start(WIDTH, height)
-
-	/*
-		for i := 0; i <= width; i = i + 50 {
-			s.Line(0, i, width, i, "fill:black;stroke:black")
-		}
-
-		for j := 0; j <= height; j = j + 10 {
-			s.Line(j, 0, j, height, "fill:black;stroke:gray;stroke-dasharray:2")
-		}
-	*/
-
 	var err error
 	Db, err = sql.Open("postgres", "user=tree1 dbname=tree1 password=tree1")
 	if err != nil {
@@ -289,19 +303,12 @@ func ThreeKidSVG(w http.ResponseWriter, req *http.Request) {
 
 	var id idcard
 
-	// s.Square(125, 125, 250, "fill:blue")
-	s.Line(375, 250, 1875, 250, "stroke:black;stroke-width:20;stroke-linecap:butt")
-	// s.Circle(2000, 250, 125, "fill:red")
+	s.Line(XCHILDORG+grid/4, YPARENTORG, XCHILDORG+(children-1)*(span+grid)-grid/4, YPARENTORG, "stroke:black;stroke-width:20;stroke-linecap:butt")
+	s.Line(XPARENTORG, YPARENTORG+grid/2, XPARENTORG+(children-1)*(span+grid), YPARENTORG+grid/2, "stroke:black;stroke-width:20;stroke-linecap:butt")
 
-	s.Line(1125, 250, 1125, 500, "stroke:black;stroke-width:20;stroke-linecap:butt")
-	s.Line(250, 500, 2000, 500, "stroke:black;stroke-width:20;stroke-linecap:butt")
-	s.Line(250, 500, 250, 750, "stroke:black;stroke-width:20;stroke-linecap:butt")
-	s.Line(1125, 500, 1125, 750, "stroke:black;stroke-width:20;stroke-linecap:butt")
-	s.Line(2000, 500, 2000, 750, "stroke:black;stroke-width:20;stroke-linecap:butt")
-
-	// s.Circle(250, 875, 125, "fill:green")
-	// s.Circle(1125, 875, 125, "fill:green")
-	// s.Circle(2000, 875, 125, "fill:green")
+	s.Line(XPARENTORG, YPARENTORG+grid/2, XPARENTORG, YCHILDORG-grid/4, "stroke:black;stroke-width:20;stroke-linecap:butt")
+	s.Line(XPARENTORG+(span+grid), YPARENTORG, XPARENTORG+(span+grid), YCHILDORG-grid/4, "stroke:black;stroke-width:20;stroke-linecap:butt")
+	s.Line(XPARENTORG+(children-1)*(span+grid), YPARENTORG+grid/2, XPARENTORG+(children-1)*(span+grid), YCHILDORG-grid/4, "stroke:black;stroke-width:20;stroke-linecap:butt")
 
 	for row.Next() {
 		err = row.Scan(&id.Id, &id.Name, &id.Age, &id.Gender,
@@ -309,64 +316,34 @@ func ThreeKidSVG(w http.ResponseWriter, req *http.Request) {
 		if err != nil {
 			return
 		}
-		//log.Println(id.Id, id.Name, id.Age, id.Gender,
-		//	id.IdNumber, id.Father, id.Mother, id.Address)
 		if id.Id == 1 {
-			s.Text(225, 250, id.Name, "text-anchor:middle;font-size:30px")
-			// log.Println(id.Age)
-			s.Text(225, 280, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
-			s.Text(225, 310, id.IdNumber, "text-anchor:middle;font-size:30px")
-			// log.Println(id.Gender)
-			s.Square(125, 125, 250, "fill:none;stroke:black")
+			s.Text(XPARENTORG, YPARENTORG, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Square(XPARENTORG-grid/4, YPARENTORG-grid/4, grid/2, "fill:none;stroke:black")
 		} else if id.Id == 2 {
-			s.Text(1975, 250, id.Name, "text-anchor:middle;font-size:30px")
-			// log.Println(id.Age)
-			s.Text(1975, 280, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
-			s.Text(1975, 310, id.IdNumber, "text-anchor:middle;font-size:30px")
-			// log.Println(id.Gender)
-			s.Circle(2000, 250, 125, "fill:none;stroke:black")
-			// log.Println(id.Id)
+			s.Text(XPARENTORG+(children-1)*(span+grid), YPARENTORG, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
+			s.Circle(XPARENTORG+(children-1)*(span+grid), YPARENTORG, grid/4, "fill:none;stroke:black")
 		} else if id.Id == 3 {
-			// log.Println(id.Age)
-			// log.Println(string(id.Age))
-			// log.Println(id.Gender)
-			// s.Circle(250, 875, 125, "fill:green")
-			s.Text(250, 875, id.Name, "text-anchor:middle;font-size:30px")
-			s.Text(250, 905, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
-			s.Text(250, 935, id.IdNumber, "text-anchor:middle;font-size:30px")
+			s.Text(XCHILDORG, YCHILDORG, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
 			if id.Gender == "男" {
-				s.Square(125, 750, 250, "fill:none;stroke:black")
+				s.Square(XCHILDORG-grid/4, YCHILDORG-grid/4, grid/2, "fill:none;stroke:black")
 			} else {
-				s.Circle(250, 875, 125, "fill:none;stroke:black")
+				s.Circle(XCHILDORG, YCHILDORG, grid/4, "fill:none;stroke:black")
 			}
 		} else if id.Id == 4 {
-			// log.Println(id.Age)
-			// log.Println(string(id.Age))
-			// log.Println(id.Gender)
-			// s.Circle(2000, 875, 125, "fill:green")
-			s.Text(1125, 875, id.Name, "text-anchor:middle;font-size:30px")
-			s.Text(1125, 905, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
-			s.Text(1125, 935, id.IdNumber, "text-anchor:middle;font-size:30px")
+			s.Text(XCHILDORG+(span+grid), YCHILDORG, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
 			if id.Gender == "男" {
-				s.Square(1000, 750, 250, "fill:none;stroke:black")
+				s.Square(XCHILDORG+(span+grid)-grid/4, YCHILDORG-grid/4, grid/2, "fill:none;stroke:black")
 			} else {
-				s.Circle(1125, 875, 125, "fill:none;stroke:black")
+				s.Circle(XCHILDORG+(span+grid), YCHILDORG, grid/4, "fill:none;stroke:black")
 			}
 
 		} else if id.Id == 5 {
-			// log.Println(id.Age)
-			// log.Println(string(id.Age))
-			// log.Println(id.Gender)
-			// s.Circle(2000, 875, 125, "fill:green")
-			s.Text(2000, 875, id.Name, "text-anchor:middle;font-size:30px")
-			s.Text(2000, 905, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
-			s.Text(2000, 935, id.IdNumber, "text-anchor:middle;font-size:30px")
+			s.Text(XCHILDORG+2*(span+grid), YCHILDORG, strconv.Itoa(id.Age), "text-anchor:middle;font-size:30px")
 			if id.Gender == "男" {
-				s.Square(1875, 750, 250, "fill:none;stroke:black")
+				s.Square(XCHILDORG+2*(span+grid)-grid/4, YCHILDORG-grid/4, grid/2, "fill:none;stroke:black")
 			} else {
-				s.Circle(2000, 875, 125, "fill:none;stroke:black")
+				s.Circle(XCHILDORG+2*(span+grid), YCHILDORG, grid/4, "fill:none;stroke:black")
 			}
-
 		}
 	}
 	s.End()
